68fae97b03b7a8f7288d6c4eb3abd66940e71ec0
omar <omarasif75@gmail.com>
2022-04-22 00:04:40 +0530

    Updated navbar

--- src/MyComponents/Navbar.js
+++ src/MyComponents/Navbar.js
@@ -9,7 +9,7 @@ export const Navbar = () => {
 
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
             <div className="container-fluid">
-              <Link className="navbar-brand" to="/">BrandName</Link>
+              <Link className="navbar-brand" to="/">Brand</Link>
               <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
               </button>
